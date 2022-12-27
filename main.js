f0abcaaedf1698869679f05d672ecdef17c3216c
D.Tamilarasan <abidm26791@gmail.com>
2022-12-27 03:14:50 -0800

    js

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 var a=new Date();
 var b=a.setTime(parseInt(prompt("Enter the time")));
-if(b>6 && b<12){
+if(b>=0 && b<=5){
+  console.log("Early Morning")
+}
+else if(b>=6 && b<=11){
   console.log("Morning")
 }
-else if(b>12 && b<16){
+else if(b>=12 && b<=15){
   console.log("Afternoon")
 }
-else if(b>16 && b<18){
+else if(b>=16 && b<=18){
   console.log("Evening")
 }
-else{
+else if(b>=19 && b<=24){
   console.log("Night")
+}
+else{
+  console.log("Not a time")
 }
 
